e31ca993f167aae1f8c728b1373dfc56132738a5
Rahul <dhankharrahul13@gmail.com>
2020-10-27 15:07:27 +0530

    train test split complete

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -14,16 +14,16 @@ predict = "G3"
 X = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
 
-x_train, x_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.1)
+x_train, x_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.1, random_state=25)
 
-print(x_train.head())
-print('-------------------------------------')
-print(x_test.head())
-print('-------------------------------------')
-print(y_train.head())
-print('-------------------------------------')
-print(y_test.head())
-print('-------------------------------------')
+# print(x_train)
+# print('-------------------------------------')
+# print(x_test)
+# print('-------------------------------------')
+# print(y_train)
+# print('-------------------------------------')
+# print(y_test)
+# print('-------------------------------------')
 
 linear = linear_model.LinearRegression()
 
@@ -37,3 +37,7 @@ print("Coefficient or m: \n", linear.coef_)
 print("Intercept or c: \n", linear.intercept_)
 
 predictions = linear.predict(x_test)
+
+print('Predicted Value------Actual Value:')
+for i in range(len(predictions)):
+    print(int(predictions[i]), y_test[i])
