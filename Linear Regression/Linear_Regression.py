7c96f93766f7615b2b69c010ee5618796f6d47de
Rahul <dhankharrahul13@gmail.com>
2020-10-27 15:56:45 +0530

    Added graphs

--- Linear Regression/Linear_Regression.py
+++ Linear Regression/Linear_Regression.py
@@ -5,9 +5,9 @@ import pickle
 from sklearn import linear_model
 from sklearn import model_selection
 from sklearn.utils import shuffle
+from matplotlib import pyplot as plt
 
 data = pd.read_csv('student-mat.csv', sep=";")
-# print(data.head())
 
 data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
 
@@ -15,27 +15,21 @@ predict = "G3"
 X = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
 
-x_train, x_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.1, random_state=25)
+x_train, x_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.1)
 
-# print(x_train)
-# print('-------------------------------------')
-# print(x_test)
-# print('-------------------------------------')
-# print(y_train)
-# print('-------------------------------------')
-# print(y_test)
-# print('-------------------------------------')
+'''
+best = 0
+for _ in range(30):
+    x_train, x_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.1)
+    linear = linear_model.LinearRegression()
+    linear.fit(x_train, y_train)
+    accuracy = linear.score(x_test, y_test)
+    print(accuracy)
 
-# linear = linear_model.LinearRegression()
-#
-# linear.fit(x_train, y_train)
-#
-# score = linear.score(x_test, y_test)
-#
-# print(score)
-#
-# with open('trained_model.pickle', 'wb') as file:
-#     pickle.dump(linear, file)
+    if accuracy > best:
+        best = accuracy
+        with open('trained_model.pickle', 'wb') as file:
+            pickle.dump(linear, file)'''
 
 with open('trained_model.pickle', 'rb') as file:
     linear = pickle.load(file)
@@ -48,3 +42,14 @@ predictions = linear.predict(x_test)
 print('Predicted Value------Actual Value:')
 for i in range(len(predictions)):
     print(int(predictions[i]), y_test[i])
+
+name = ["G1", "G2", "studytime", "failures", "absences"]
+
+
+for i in range(5):
+    plt.subplot(2, 3, i + 1)
+    plt.scatter(data[name[i]], data["G3"], c="blue", s=5)
+    plt.xlabel(name[i], fontsize=10, labelpad=-2)
+    plt.ylabel("Final Marks")
+
+plt.show()
